de454261e29e538978d0af100d86b2718c313137
Zacharia Mwaura <75284078+ZachariaMwaura@users.noreply.github.com>
2024-01-03 18:50:51 -0500

    Initial Commit

---
+++ Magic_8_Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_8_Ball
+//
+//  Created by Zach Mwaura on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
